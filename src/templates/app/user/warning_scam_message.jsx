600974b4bd725a38bdedf52e926c89215a5a1284
tay suisin <suisin@regentmarkets.com>
2022-09-14 19:01:56 +0800

    Update icons and scss for popup

--- src/templates/app/user/warning_scam_message.jsx
+++ src/templates/app/user/warning_scam_message.jsx
@@ -4,15 +4,17 @@ const WarningScamMessage = () => (
     <React.Fragment>
         <div id='warning_scam_message' className='lightbox invisible'>
             <div id='warning_scam_message_content'>
-                <h1>{it.L('Beware of the fake links.')}</h1>
-                <p>{it.L('A link can contain the word "Binary" and still be fake.')}</p>
+                <h1 id='warning_scam_message_title'>{it.L('Beware of the fake links.')}</h1>
+                <p id='warning_scam_message_subtitle'>{it.L('A link can contain the word "Binary" and still be fake.')}</p>
                 <h2 id='warning_scam_message_title_secondary'>{it.L('Do not get lured to fake "Binary" pages!')}</h2>
                 <p id='warning_scam_message_text_secondary'>{it.L('You may see links to websites with a fake Binary login page where you’ll get scammed for your money. ')}</p>
                 <div className='warning-scam-message-content-info-message'>
+                    <img className='icon-sm margin-right-0' src={it.url_for('images/pages/warning_scam_message/ic-account-cross.svg')} />
                     <h3 id='warning_scam_message_scam_title_text'>{it.L('Do not trust and give away your credentials on fake websites, ads or emails.')}</h3>
                 </div>
                 <div className='warning-scam-message-correct-content'>
                     <div className='warning-scam-message-content-info-message'>
+                        <img className='icon-sm margin-left-0' src={it.url_for('images/pages/warning_scam_message/ic-account-tick.svg')} />
                         <h3 id='warning_scam_message_scam_title_text'>
                             {it.L('Only log in to your account at this secure link, never elsewhere.')}
                         </h3>
@@ -23,6 +25,7 @@ const WarningScamMessage = () => (
                 </div>
                 <div className='warning-scam-message-wrong-content'>
                     <div className='warning-scam-message-content-info-message'>
+                        <img className='icon-sm margin-left-0' src={it.url_for('images/pages/warning_scam_message/ic-account-cross.svg')} />
                         <h3 id='warning_scam_message_scam_title_text'>
                             {it.L('Fake links often contain the word that looks like "Binary" but look out for these differences.')}
                         </h3>
@@ -33,9 +36,10 @@ const WarningScamMessage = () => (
                 </div>
                 <div className='warning-scam-message-checkbox-container'>
                     <input type='checkbox' id='warning_scam_message_checkbox' />
-                    <label htmlFor='warning_scam_message_checkbox'>{it.L('I\'ve read the above carefully.')}</label>
+                    <label htmlFor='warning_scam_message_checkbox' id='warning_scam_message_acknowledge_message'>{it.L('I\'ve read the above carefully.')}</label>
                 </div>
                 <div className='warning-scam-message-button-icon-container'>
+                    <img src={it.url_for('images/pages/warning_scam_message/ic-account-dont-get-scam.svg')} />
                     <button id='warning_scam_message_button' className='button-disabled'>{it.L('OK, got it')}</button>
                 </div>
             </div>
